19a5f36d2e87cdab6680991e2ceb08416d8f46bc
MuhammadAliKhalid <alikhalidramay@gmail.com>
2024-10-21 15:48:19 +0500

    QF - SR to ID for ComparisonController

--- app/Http/Controllers/ComparisonController.php
+++ app/Http/Controllers/ComparisonController.php
@@ -11,43 +11,43 @@ class ComparisonController extends Controller
 {
     public function main()
     {
-        $allCategories = Tree::pluck('title', 'sr')->all();
+        $allCategories = Tree::pluck('title', 'id')->all();
         return view('comparison.main', compact('allCategories'));
     }
     public function middle($class)
     {
-        $allCategories = Tree::pluck('title', 'sr')->all();
+        $allCategories = Tree::pluck('title', 'id')->all();
         return view('comparison.middle', compact('allCategories', 'class'));
     }
     public function naseehaClasses()
     {
 
-        $allCategories = Tree::pluck('title', 'sr')->all();
+        $allCategories = Tree::pluck('title', 'id')->all();
         return view('comparison.naseehaClasses', compact('allCategories'));
     }
     public function naseeha($class)
     {
-        $categories = Tree::whereRelation('detail', 'age', '=', $class)->orderBy('sr', 'asc')->get();
-        $allCategories = Tree::pluck('title', 'sr')->all();
+        $categories = Tree::whereRelation('detail', 'age', '=', $class)->orderBy('id', 'asc')->get();
+        $allCategories = Tree::pluck('title', 'id')->all();
         return view('comparison.naseeha', compact('categories', 'allCategories'));
     }
 
     public function maktabClasses()
     {
-        $allCategories = Tree::pluck('title', 'sr')->all();
+        $allCategories = Tree::pluck('title', 'id')->all();
         return view('comparison.maktabClasses', compact('allCategories'));
     }
 
     public function maktab($class)
     {
-        $categories = Tree::whereRelation('detail', 'age', '=', $class)->orderBy('sr', 'asc')->get();
-        $allCategories = Tree::pluck('title', 'sr')->all();
+        $categories = Tree::whereRelation('detail', 'age', '=', $class)->orderBy('id', 'asc')->get();
+        $allCategories = Tree::pluck('title', 'id')->all();
         return view('comparison.maktab', compact('categories', 'allCategories'));
     }
 
     public function governmentClasses()
     {
-        $allCategories = Tree::pluck('title', 'sr')->all();
+        $allCategories = Tree::pluck('title', 'id')->all();
         return view('comparison.governmentClasses', compact('allCategories'));
     }
     public function governmentBasic($class)
@@ -61,13 +61,13 @@ class ComparisonController extends Controller
                     ->orWhere('hukam', '=', 'بنیاد')
                     ->orWhere('hukam', '=', 'مکروہ تحریمی');
             })
-            ->orderBy('sr', 'asc')->get();
+            ->orderBy('id', 'asc')->get();
         return view('comparison.government', compact('categories'));
     }
     public function governmentDetailed($class)
     {
-        $categories = Tree::whereRelation('mahol', 'parhana', '=', $class)->orderBy('sr', 'asc')->get();
-        $allCategories = Tree::pluck('title', 'sr')->all();
+        $categories = Tree::whereRelation('mahol', 'parhana', '=', $class)->orderBy('id', 'asc')->get();
+        $allCategories = Tree::pluck('title', 'id')->all();
         return view('comparison.government', compact('categories', 'allCategories'));
     }
 }
